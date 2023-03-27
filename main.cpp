093f2294402420418eafa909cc3735cc34fe4105
HansSeanNathanael <hans.sean22@gmail.com>
2023-03-27 22:27:26 +0700

    Modifikasi input dengan file

--- main.cpp
+++ main.cpp
@@ -85,40 +85,75 @@ pair<vector<int>, vector<int>> path(const vector<int>& constraint, vector<unorde
 }
 
 
+
+
 int main() {
 //    const vector<int>& constraint, vector<unordered_map<int, vector<int>>> edges, int source, int destination
-    vector<int> constraint = {0, 10};
-    vector<unordered_map<int, vector<int>>> edges({
-        unordered_map<int, vector<int>>({
-            {1, vector<int>({11,4})},
-            {2, vector<int>({18,9})},
-            {3, vector<int>({4,4})}
-        }),
-        unordered_map<int, vector<int>>({
-            {0, vector<int>({10,6})},
-            {2, vector<int>({3,2})},
-            {5, vector<int>({14,7})}
-        }),
-        unordered_map<int, vector<int>>({
-            {0, vector<int>({8,6})},
-            {1, vector<int>({3,3})},
-            {4, vector<int>({4,3})}
-        }),
-        unordered_map<int, vector<int>>({
-            {0, vector<int>({5,3})},
-            {4, vector<int>({15,11})}
-        }),
-        unordered_map<int, vector<int>>({
-            {2, vector<int>({3,3})},
-            {3, vector<int>({15,12})},
-            {5, vector<int>({6,3})}
-        }),
-        unordered_map<int, vector<int>>({
-            {1, vector<int>({14,22})},
-            {2, vector<int>({7,6})},
-            {4, vector<int>({7,5})}
-        })
-    });
+    int vertex_count = 0;
+    int edge_count = 0;
+    int constraint_count = 0;
+
+    // input baris pertama adalah 3 angka yaitu jumlah vertex (V), jumlah edge (E), dan jumlah constraint (C)
+    // input E (jumlah edge) baris selanjutnya adalah data vertex yang formatnya adalah
+    // VERTEX_AWAL VERTEX_AKHIR kemudian diikuti oleh C + 1 input yaitu COST_JARAK COST_CONSTRAINT_1 COST_CONSTRAINT_2 ... COST_CONSTRAINT_C
+    // input baris terakhir adalah maksimum cost setiap constraint, jadi ada C input angka maksimal cost constraint.
+    FILE* file_input = fopen("input.txt", "r");
+    fscanf(file_input, "%d %d %d", &vertex_count, &edge_count, &constraint_count);
+
+    vector<int> constraint(constraint_count + 1);
+    vector<unordered_map<int, vector<int>>> edges(vertex_count);
+
+//    vector<unordered_map<int, vector<int>>> edges({
+//        unordered_map<int, vector<int>>({
+//            {1, vector<int>({11,4})},
+//            {2, vector<int>({18,9})},
+//            {3, vector<int>({4,4})}
+//        }),
+//        unordered_map<int, vector<int>>({
+//            {0, vector<int>({10,6})},
+//            {2, vector<int>({3,2})},
+//            {5, vector<int>({14,7})}
+//        }),
+//        unordered_map<int, vector<int>>({
+//            {0, vector<int>({8,6})},
+//            {1, vector<int>({3,3})},
+//            {4, vector<int>({4,3})}
+//        }),
+//        unordered_map<int, vector<int>>({
+//            {0, vector<int>({5,3})},
+//            {4, vector<int>({15,11})}
+//        }),
+//        unordered_map<int, vector<int>>({
+//            {2, vector<int>({3,3})},
+//            {3, vector<int>({15,12})},
+//            {5, vector<int>({6,3})}
+//        }),
+//        unordered_map<int, vector<int>>({
+//            {1, vector<int>({14,22})},
+//            {2, vector<int>({7,6})},
+//            {4, vector<int>({7,5})}
+//        })
+//    });
+
+    for (int i = 0; i < edge_count; i++) {
+        int awal, akhir;
+        fscanf(file_input, "%d %d", &awal, &akhir);
+
+        vector<int> costs;
+        for (int j = 0; j < constraint_count + 1; j++) {
+            int cost;
+            fscanf(file_input, "%d", &cost);
+            costs.push_back(cost);
+        }
+
+        edges[awal].insert({akhir, costs});
+    }
+
+    for (int i = 1; i < constraint_count + 1; i++) {
+        int cost;
+        fscanf(file_input, "%d", &cost);
+        constraint[i] = cost;
+    }
 
 
     pair<vector<int>, vector<int>> result = path(constraint, edges, 0, 5);
